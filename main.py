f591b54085faedc94ece9650cc12fa9e74c7daa7
Boudjada Yasser <by.root96@gmail.com>
2023-01-09 18:05:19 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-
-import sys
-
-
 from datetime import datetime
 now = datetime.now()
 
 f=open("README.md", "w+")
 f.write(f'last update at : {now.strftime("%d/%m/%Y %H:%M:%S")} (UTC)')
 f.close()
-print('False')
+print(0)
